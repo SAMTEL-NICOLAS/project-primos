2c7766e015f6cdeac29dae74b775770d664f748f
Jesus Sierra <jnsierrac@gmail.com>
2018-11-19 10:12:21 -0500

    001 Ajustes para el despliegue en wildfly

--- src/main/java/co/com/samtel/App.java
+++ src/main/java/co/com/samtel/App.java
@@ -2,12 +2,21 @@ package co.com.samtel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App  extends SpringBootServletInitializer{
+	
+	private static Class<App> applicationClass = App.class;
 	
 	public static void main(String ...  args) {
 		SpringApplication.run(App.class, args);
 	}
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(applicationClass);
+    }
 
 }
